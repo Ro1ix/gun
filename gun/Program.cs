b22eb5fcfa2f606b4990ade6cc5d514b32fc6a04
Rolix <130452450+Ro1ix@users.noreply.github.com>
2023-05-31 09:40:37 +0300

    v1.1
    Добавлена наивысшая точка траектории

--- gun/Program.cs
+++ gun/Program.cs
@@ -28,18 +28,22 @@ namespace gun
                 double Vy0 = V0 * Math.Sin((a * Math.PI) / 180);
                 double x = Math.Round(x0 + Vx0 * t, 2);
                 double y = Math.Round(y0 + Vy0 * t - (9.81 * Math.Pow(t, 2)) / 2, 2);
-                double max = y0;
+                double max_x = x0;
+                double max_y = y0;
                 Console.WriteLine("\n  X         Y");
                 for (t = 0; y >= 0; t++)
                 {
                     x = Math.Round(x0 + Vx0 * t, 2);
                     y = Math.Round(y0 + Vy0 * t - (9.81 * Math.Pow(t, 2)) / 2, 2);
-                    if (max < y)
-                        max = y;
+                    if (max_y < y)
+                    {
+                        max_x = x;
+                        max_y = y;
+                    }
                     if (y >= 0)
                         Console.WriteLine($"{x}\t{y}");
                 }
-                Console.WriteLine($"\nНаивысшая точка по Y - {max}");
+                Console.WriteLine($"\nКоординаты наивысшей точки траектории снаряда: {max_x}\t{max_y}");
             }
         }
     }
